472279496c2c115bcb11934b00a1edd20854a392
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-02-11 21:50:59 +0330

    Writing to JSON added

--- main.rb
+++ main.rb
@@ -5,10 +5,21 @@ require 'time'
 set :bind, '0.0.0.0' 
 
 a = [] # creating an empty array as the main skeleton of the JSON 
-json_data = File.new("data.json")
-json_data.close() 
+
 
 get '/' do 
  
- "Welcome to the file storing API. This one uses a JSON file instead of a DB". 
+ "Welcome to the file storing API. This one uses a JSON file instead of a DB"
+
 end
+
+get '/:temperature/:humidity' do 
+
+ a << {"date" => "#{Time.now().month()}/#{Time.now().day()}/#{Time.now().year()}", "temperature" => params[:temperature], "humidity" => params[:humidity]} 
+ 
+ temp_change = File.open("data.json", "w+") 
+ temp_change.write(a.to_json) 
+ temp_change.close() 
+
+ 'successfully written to the file' 
+end 
