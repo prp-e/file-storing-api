853576c21abaaf4a78502c5909bcd3b63726b8c9
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-02-11 21:37:04 +0330

    Added necessary files

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 require 'sinatra' 
 require 'json' 
+require 'time'
 
 set :bind, '0.0.0.0' 
+
+a = [] # creating an empty array as the main skeleton of the JSON 
+json_data = File.new("data.json")
+json_data.close() 
 
 get '/' do 
  
